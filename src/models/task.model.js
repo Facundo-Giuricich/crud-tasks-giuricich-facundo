75a29ef46a555c704c7a4555ba2b3538a5c0a09d
Facundo Giuricich <facundogiuricich936@gmail.com>
2025-08-18 10:30:13 -0300

    modelo task con relación user agregado

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -1,11 +1,20 @@
 import { DataTypes } from 'sequelize';
-import {sequelize} from '../config/database.js';
+import { sequelize } from '../config/database.js';
+import { User } from './user.model.js';
 
 const Task = sequelize.define('Task', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   title: { type: DataTypes.STRING(100), allowNull: false, unique: true },
   description: { type: DataTypes.STRING(100), allowNull: false },
-  isComplete: { type: DataTypes.BOOLEAN, defaultValue: false }
+  isComplete: { type: DataTypes.BOOLEAN, defaultValue: false },
+  user_id: { type: DataTypes.INTEGER, allowNull: false }
+}, {
+  tableName: 'tasks',
+  timestamps: false
 });
 
-export default Task;
+// Relaciones
+User.hasMany(Task, { foreignKey: 'user_id', as: 'tasks' });
+Task.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
+
+export { Task };
