245f16efdfa345b4e0a4fa1feca5c3c78231b78c
Facundo Giuricich <facundogiuricich936@gmail.com>
2025-08-18 10:34:35 -0300

    rutas agregadas para tareas (crear-listar)

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,27 +1,9 @@
 import { Router } from 'express';
-import {
-  createTask,
-  getTasks,
-  getTaskById,
-  updateTask,
-  deleteTask
-} from '../controllers/task.controller.js';
+import { createTask, getTasks } from '../controllers/task.controller.js';
 
 const router = Router();
 
-// Crear tarea
-router.post('/', createTask);
-
-// Obtener todas las tareas
-router.get('/', getTasks);
-
-// Obtener una tarea por ID
-router.get('/:id', getTaskById);
-
-// Actualizar tarea por ID
-router.put('/:id', updateTask);
-
-//  Eliminar tarea por ID
-router.delete('/:id', deleteTask);
+router.post('/tasks', createTask);
+router.get('/tasks', getTasks);
 
 export default router;
