807119f7c46ba9c21e4d70434d9e20edd28fed2e
Facundo Giuricich <facundogiuricich936@gmail.com>
2025-08-18 10:27:52 -0300

    modelo user agregado

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,11 +1,14 @@
 import { DataTypes } from 'sequelize';
-import {sequelize} from '../config/database.js';
+import { sequelize } from '../config/database.js';
 
 const User = sequelize.define('User', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   name: { type: DataTypes.STRING(100), allowNull: false },
   email: { type: DataTypes.STRING(100), allowNull: false, unique: true },
   password: { type: DataTypes.STRING(100), allowNull: false }
+}, {
+  tableName: 'users',
+  timestamps: false
 });
 
-export default User;
+export {User};
