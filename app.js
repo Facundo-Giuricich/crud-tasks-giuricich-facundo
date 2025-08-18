6bf0070eabec0e1dfcfe6ab55f0635317cf9a030
Facundo Giuricich <facundogiuricich936@gmail.com>
2025-08-18 10:36:49 -0300

    2

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 import express from 'express';
-import { sequelize } from './src/config/database.js';
-import usersRoutes from './src/routes/user.routes.js';
-import tasksRoutes from './src/routes/task.routes.js';
+import taskRoutes from './src/routes/task.routes.js';
 
 const app = express();
 app.use(express.json());
 
-app.use('/api/users', usersRoutes);
-app.use('/api/tasks', tasksRoutes);
+app.use('/api', taskRoutes);
 
-// Sincroniza modelos
-sequelize.sync()
-  .then(() => console.log('Base de datos sincronizada'))
-  .catch((err) => console.error('Error al sincronizar la base de datos', err));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
-});
+export default app;
