f03d6bca312a3c081d302e99e53bf7750f537fe3
Facundo Giuricich <facundogiuricich936@gmail.com>
2025-08-24 19:51:24 -0300

    Estructura final y configuración completa

--- app.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import taskRoutes from './src/routes/task.routes.js';
-
-const app = express();
-app.use(express.json());
-
-app.use('/api', taskRoutes);
-
-export default app ;
